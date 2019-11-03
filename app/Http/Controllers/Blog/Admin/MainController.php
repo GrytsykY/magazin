6372eea5ee0862f9c344879b67a521c0b825a3b4
Yurii Grytsyk <grytsykyura@gmail.com>
2019-11-03 20:19:59 +0200

    Create AdminLTE, Bread Crumbs, Meta Tags

--- app/Http/Controllers/Blog/Admin/MainController.php
+++ app/Http/Controllers/Blog/Admin/MainController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\Blog\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use MetaTag;
 
 class MainController extends AdminBaseController
 {
     public function index(){
+        MetaTag::setTags(['title' => 'Админ панель']);
         return view('blog.admin.main.index');
     }
 }
