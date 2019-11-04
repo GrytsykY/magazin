6d219d1f724b847866b41544d12ae777ccb9e33a
Yurii Grytsyk <grytsykyura@gmail.com>
2019-11-04 17:34:46 +0200

    Create Admin_Order_Save_Repository, Delete,

--- resources/views/blog/admin/components/result_messages.blade.php
+++ resources/views/blog/admin/components/result_messages.blade.php
@@ -15,11 +15,13 @@
 @endif
 @if(session('success'))
     <div class="row justify-content-center">
-        <div class="col-md-11">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true"></span>
-            </button>
-            {{session()->get('success')}}
+        <div class="col-md-11 ">
+            <div class="alert alert-success" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true"></span>
+                </button>
+                {{session()->get('success')}}
+            </div>
         </div>
     </div>
 @endif
