b1c97c50ba443ffa33f9a93b76d344d349f80eb7
Yurii Grytsyk <grytsykyura@gmail.com>
2019-11-15 10:41:44 +0200

    Edit Delete Filter Group

---
+++ app/Repositories/Admin/CurrencyRepository.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace App\Repositories\Admin;
+
+use App\Models\Admin\Currency as Model;
+use App\Models\Admin\Currency;
+use App\Repositories\CoreRepository;
+
+class CurrencyRepository extends CoreRepository
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    public function getAllCurrency()
+    {
+        $currency = $this->startConditions()::all();
+        return $currency;
+    }
+
+    public function switchBaseCurr()
+    {
+        $id = \DB::table('currencies')
+            ->where('base','=','1')
+            ->get()
+            ->first();
+        if ($id){
+            $id = $id->id;
+            $new = Currency::find($id);
+            $new->base = '0';
+            $new->save();
+        }else{
+            return back()
+                ->withErrors(['msg'=>"Ошибка Базовой валюты еще нет"])
+                ->withInput();
+        }
+
+    }
+}
